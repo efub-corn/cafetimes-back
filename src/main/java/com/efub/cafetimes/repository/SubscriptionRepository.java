c37c18838c09396c5899ad9ea13c86bcb2c811ca
Gom3rye <kimmyeongji3@gmail.com>
2022-11-08 18:21:30 +0900

    Test: subscription 조회

--- src/main/java/com/efub/cafetimes/repository/SubscriptionRepository.java
+++ src/main/java/com/efub/cafetimes/repository/SubscriptionRepository.java
@@ -3,6 +3,24 @@ package com.efub.cafetimes.repository;
 import com.efub.cafetimes.domain.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
+    //쿼리 메소드 이용
+    List<Subscription> findByMenu(String menu);
+    List<Subscription> findByMenuOrSubscriptionDetail(String menu, String subscriptionDetail);
+    List<Subscription> findByPriceLessThan(Integer price);
+    List<Subscription> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    //@Query로 JPQL 이용
+    @Query("select i from Subscription i where i.subscriptionDetail like %:subscriptionDetail% order by i.price desc")
+    List<Subscription> findBySubscriptionDetail(@Param("subscriptionDetail") String subscriptionDetail);
+
+    @Query(value="select * from subscription i where i.subscription_detail like %:subscriptionDetail% order by i.price desc", nativeQuery = true)
+    List<Subscription> findBySubscriptionDetailByNative(@Param("subscriptionDetail") String subscriptionDetail);
 }
--- src/test/java/com/efub/cafetimes/repository/SubscriptionRepositoryTest.java
+++ src/test/java/com/efub/cafetimes/repository/SubscriptionRepositoryTest.java
@@ -10,8 +10,13 @@ import org.springframework.test.context.TestPropertySource;
 
 import javax.persistence.Column;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAQuery;
+//import com.efub.cafetimes.domain.QItem;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -39,5 +44,83 @@ class SubscriptionRepositoryTest {
         System.out.println(savedSubscription.toString());
 
     }
+
+    public void createSubscriptionList(){
+        for(int i=1; i<=10; i++){
+            Subscription subscription = new Subscription();
+            subscription.setMenu("테스트 구독권"+i);
+            subscription.setPrice(100000+i);
+            subscription.setCurrentAmount(33);
+            subscription.setTotalAmount(50);
+            subscription.setTerm(2);
+            subscription.setSubscriptionDetail("테스트 구독권 상세 설명"+i);
+            subscription.setSubscriptionSellStatus(SubscriptionSellStatus.SELL);
+            subscription.setStockNumber(100);
+            subscription.setCreatedAt(LocalDateTime.now());
+            subscription.setUpdateTime(LocalDateTime.now());
+            Subscription savedSubscription = subscriptionRepository.save(subscription);
+        }
+    }
+
+    @Test
+    @DisplayName("구독권 조회 테스트")
+    public void findByMenuTest(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findByMenu("테스트 구독권1");
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("구독권명, 구독권상세설명 or 테스트")
+    public void findByMenuOrSubscriptionDetailTest(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findByMenuOrSubscriptionDetail("테스트 구독권1", "테스트 구독권 상세 설명5");
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findByPriceLessThan(100005);
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findByPriceLessThanOrderByPriceDesc(100005);
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 구독권 조회 테스트")
+    public void findBySubscriptionDetailTest(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findBySubscriptionDetail("테스트 구독권 상세 설명");
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@nativeQuery 속성을 이용한 구독권 조회 테스트")
+    public void findBySubscriptionDetailByNative(){
+        this.createSubscriptionList();
+        List<Subscription> subscriptionList = subscriptionRepository.findBySubscriptionDetailByNative("테스트 구독권 상세 설명");
+        for(Subscription subscription : subscriptionList){
+            System.out.println(subscription.toString());
+        }
+    }
+
 
 }
