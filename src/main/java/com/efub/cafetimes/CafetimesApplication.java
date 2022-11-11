c358cfbbc23834c68f7de751828903d09cc8295c
YOONJI KIM <stritegdc@gmail.com>
2022-11-11 14:18:31 +0900

    Fix: default time zone 설정

--- src/main/java/com/efub/cafetimes/CafetimesApplication.java
+++ src/main/java/com/efub/cafetimes/CafetimesApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class CafetimesApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafetimesApplication.class, args);
