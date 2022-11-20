929f5bf9f1eb05624f5bc37fd87b1ad96d593658
Gom3rye <kimmyeongji3@gmail.com>
2022-11-20 14:56:00 +0900

    Refactor: Store -> Cafe로 변경

--- src/main/java/com/efub/cafetimes/domain/Cafe.java
+++ src/main/java/com/efub/cafetimes/domain/Cafe.java
@@ -11,11 +11,11 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name="store")
-public class Store extends BaseEntity{
+@Table(name="cafe")
+public class Cafe extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long storeId;
+    private Long cafeId;
 
     @ManyToOne
     @NotNull
@@ -24,7 +24,7 @@ public class Store extends BaseEntity{
 
     @Column(columnDefinition = "TEXT")
     @NotNull
-    private String storeName;
+    private String cafeName;
 
     @Column(columnDefinition = "TEXT")
     @NotNull
@@ -43,9 +43,9 @@ public class Store extends BaseEntity{
     private Integer subscriptionCount;
 
     @Builder
-    public Store(User owner, String storeName, String address, String businessInfo, String image, Boolean isConfirmed, Integer subscriptionCount) {
+    public Cafe(User owner, String cafeName, String address, String businessInfo, String image, Boolean isConfirmed, Integer subscriptionCount) {
         this.owner = owner;
-        this.storeName = storeName;
+        this.cafeName = cafeName;
         this.address = address;
         this.businessInfo = businessInfo;
         this.image = image;
--- src/main/java/com/efub/cafetimes/dto/CafeDto.java
+++ src/main/java/com/efub/cafetimes/dto/CafeDto.java
@@ -1,20 +1,15 @@
 package com.efub.cafetimes.dto;
 
 import com.efub.cafetimes.domain.User;
-import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 @Getter
 @Setter
-public class StoreDto {
-    private Long storeId;
+public class CafeDto {
+    private Long cafeId;
     private User owner;
-    private String storeName;
+    private String cafeName;
     private String address;
     private String businessInfo;
     private String image;
