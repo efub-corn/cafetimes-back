6e92e9220d52497283d899e4c5f7dbc8f6b0a1ca
Gom3rye <kimmyeongji3@gmail.com>
2022-11-22 16:07:05 +0900

    Refactor: UserRepository 코드 추가

--- src/main/java/com/efub/cafetimes/repository/UserRepository.java
+++ src/main/java/com/efub/cafetimes/repository/UserRepository.java
@@ -4,10 +4,14 @@ import com.efub.cafetimes.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByKakaoId(Long kakaoId);
     User findByEmail(String email);
-
+    List<User> findUsersByUserIdAndAndDeleteFlagIsFalse(Long userId);
+    Optional<User> findUserByUserIdAndDeleteFlagIsFalse(Long userId);
 }
